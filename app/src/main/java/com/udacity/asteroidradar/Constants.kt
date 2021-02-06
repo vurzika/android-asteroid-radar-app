3e8574fc5ef71a7ac8bf4fdebfedf56d1ca33843
Viktorija Urzika <viktorija.urzika@gmail.com>
2021-02-05 16:41:41 -0800

    Cleanup application UI styles to match requirements

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,6 +5,4 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov"
     const val API_KEY_VALUE = "KYa7PulwMDMmVAvKdqIlnksvrcpXVBXksR5pjxpt"
-
-    // https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-02-10&end_date=2021-02-17&api_key=KYa7PulwMDMmVAvKdqIlnksvrcpXVBXksR5pjxpt
 }
