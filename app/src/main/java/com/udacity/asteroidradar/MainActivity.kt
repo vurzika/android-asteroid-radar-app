deaeb2145afac456a0fc3daf65526ffacbee268b
Viktorija Urzika <viktorija.urzika@gmail.com>
2021-02-05 16:39:24 -0800

    Introduce custom Toolbar and Up button

--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -2,6 +2,9 @@ package com.udacity.asteroidradar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.udacity.asteroidradar.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,6 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        // connect custom toolbar with navigation controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.toolbar
+            .setupWithNavController(navController)
     }
 }
+
